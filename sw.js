498892ef252fe5d3e9cb3139f0976932b07a6d78
ChasingMajors <john@chasingmajors.com>
2026-03-01 19:43:35 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cm-checklist-beta-v3";
+const CACHE = "cm-checklist-beta-v4";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
