771976dca2342d9b769ccdda5c8ade67a6648fbb
ChasingMajors <john@chasingmajors.com>
2026-03-01 18:47:51 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,21 @@
-const CACHE = "cm-checklist-beta-v2";
+const CACHE = "cm-checklist-beta-v3";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  const url = new URL(req.url);
-
-  // Only cache same-origin app files
+  const url = new URL(event.request.url);
   if (url.origin === location.origin) {
-    event.respondWith(
-      caches.match(req).then((cached) => cached || fetch(req))
-    );
+    event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
   }
 });
